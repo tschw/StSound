c166de548453d35b9b843d29f27751b399f7e988
Tobias Schwinger <tschwinger@elitemail.org>
2025-05-27 00:34:56 +0100

    Enhance command line interface of SmallYmPlayer
    It can now play multiple files and behaves like `mpg123`: If multiple
    files are given, Ctrl-C skips to the next, hit it twice to quit.

--- SmallYmPlayer/SmallYmPlayer.cpp
+++ SmallYmPlayer/SmallYmPlayer.cpp
@@ -37,10 +37,11 @@
 -----------------------------------------------------------------------------*/
 
 #include <windows.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <conio.h>
+#include <unistd.h>
 #include "SoundServer.h"
 #include "../StSoundLibrary/StSoundLibrary.h"
 
@@ -49,9 +50,14 @@
 static	volatile	YMMUSIC			*	s_pMusic = NULL;
 static	CSoundServer					soundServer;
 
+static	volatile	bool				ctrl_c = false;
+static	void		termHandler(int)
+{
+	ctrl_c = true;
+	signal(SIGINT, &termHandler);
+}
 
-
-static	void	soundServerCallback(void *pBuffer,long size)
+static	void		soundServerCallback(void *pBuffer,long size)
 {
 
 	if (s_pMusic)
@@ -59,6 +65,86 @@ static	void	soundServerCallback(void *pBuffer,long size)
 		int nbSample = size / sizeof(ymsample);
 		ymMusicCompute((void*)s_pMusic,(ymsample*)pBuffer,nbSample);
 	}
+}
+
+enum endstate_t
+{
+	endstate_ok,
+	endstate_abort,
+	endstate_file,
+	endstate_fatal
+};
+
+static	endstate_t	do_file(const char* pFileName, bool loop)
+{
+
+	endstate_t result = endstate_ok;
+
+	printf("Loading music \"%s\"...\n",pFileName);
+	YMMUSIC *pMusic = ymMusicCreate();
+
+	if (ymMusicLoad(pMusic,pFileName))
+	{
+		if (soundServer.open(soundServerCallback,500))
+		{
+
+			ymMusicInfo_t info;
+			ymMusicGetInfo(pMusic,&info);
+			printf("Name.....: %s\n",info.pSongName);
+			printf("Author...: %s\n",info.pSongAuthor);
+			printf("Comment..: %s\n",info.pSongComment);
+			printf("Duration.: %d:%02d\n",info.musicTimeInSec/60,info.musicTimeInSec%60);
+
+			printf("\nPlaying music...(press Ctrl-C to abort)\n");
+
+			signal(SIGINT, &termHandler);
+			ymMusicSetLoopMode(pMusic,loop?YMTRUE:YMFALSE);
+			ymMusicPlay(pMusic);
+			s_pMusic = pMusic;			// global instance for soundserver callback
+
+			int oldSec = -1;
+			bool finished = false;
+			while (!finished)
+			{
+				if (ctrl_c)
+				{
+					ctrl_c = false;
+					break;
+				}
+
+				int sec = ymMusicGetPos(pMusic) / 1000;
+				if (sec != oldSec)
+				{
+					printf("Time: %d:%02d\r",sec/60,sec%60);
+					oldSec = sec;
+				}
+				sleep(1);
+
+				if (!loop && sec == info.musicTimeInSec) finished = true;
+			}
+			printf("\n");
+
+			// Switch off replayer
+			s_pMusic = NULL;
+			ymMusicStop(pMusic);
+			soundServer.close();
+			result = finished?endstate_ok:endstate_abort;
+		}
+		else
+		{
+			printf("ERROR: Unable to initialize sound card hardware\n");
+			result = endstate_fatal;
+		}
+	}
+	else
+	{
+		printf("Error in loading file %s:\n%s\n",pFileName,ymMusicGetLastError(pMusic));
+		result = endstate_file;
+	}
+
+	ymMusicDestroy(pMusic);
+
+	return result;
 }
 
 
@@ -74,75 +160,36 @@ int main(int argc, char* argv[])
 	printf(	"SmallYmPlayer.\n"
 			"Using ST-Sound Library, under BSD license\n"
 			"Copyright (C) 1995-1999 Arnaud Carre ( http://leonard.oxg.free.fr )\n");
-	
-	if (argc!=2)
+
+	if (argc<2)
 	{
-		printf("Usage: SmallYmPlayer <ym music file>\n\n");
+		printf("Usage: SmallYmPlayer <ym music files>\n\n");
 		return -1;
 	}
 
-
 	//--------------------------------------------------------------------------
-	// Load YM music and creates WAV file
+	// Play YM music in a loop
 	//--------------------------------------------------------------------------
-	printf("Loading music \"%s\"...\n",argv[1]);
-	YMMUSIC *pMusic = ymMusicCreate();
-
-	if (ymMusicLoad(pMusic,argv[1]))
+	bool looping_a_single_file = (argc==2);
+	do
 	{
-		if (soundServer.open(soundServerCallback,500))
+		for (int i = 1; i < argc; ++i)
 		{
-
-			ymMusicInfo_t info;
-			ymMusicGetInfo(pMusic,&info);
-			printf("Name.....: %s\n",info.pSongName);
-			printf("Author...: %s\n",info.pSongAuthor);
-			printf("Comment..: %s\n",info.pSongComment);
-			printf("Duration.: %d:%02d\n",info.musicTimeInSec/60,info.musicTimeInSec%60);
-			printf("Driver...: %s\n", info.pSongPlayer);
-
-			printf("\nPlaying music...(press a key to abort)\n");
-
-			ymMusicSetLoopMode(pMusic,YMTRUE);
-			ymMusicPlay(pMusic);
-			s_pMusic = pMusic;			// global instance for soundserver callback
-
-			int oldSec = -1;
-			for (;;)
+			switch (do_file(argv[i],looping_a_single_file))
 			{
-				if (_kbhit())
+				case endstate_ok:
 					break;
-
-				int sec = ymMusicGetPos(pMusic) / 1000;
-				if (sec != oldSec)
-				{
-					printf("Time: %d:%02d\r",sec/60,sec%60);
-					oldSec = sec;
-				}
-
-				Sleep(20);
+				case endstate_abort:
+					sleep(1);
+					if (ctrl_c) return 0;
+					break;
+				case endstate_fatal:
+				case endstate_file:
+					return -1;
 			}
-			_getch();		// flush the key pressed
-			printf("\n");
-
-			// Switch off replayer
-			s_pMusic = NULL;
-			ymMusicStop(pMusic);
-			soundServer.close();
-
-		}
-		else
-		{
-			printf("ERROR: Unable to initialize sound card hardware\n");
 		}
 
-	}
-	else
-	{
-		printf("Error in loading file %s:\n%s\n",argv[1],ymMusicGetLastError(pMusic));
-	}
-
-	ymMusicDestroy(pMusic);
+	} while (!looping_a_single_file && !ctrl_c);
 
 	return 0;
 }
@@ -153,7 +200,7 @@ int main(int argc, char* argv[])
 int main(int argc, char* argv[])
 {
 	printf(	"SmallYmPlayer only run on Windows, sorry!\n");
-	return 0;	
+	return 0;
 }
 
 #endif
